b478cc18c64dcb850d292355d785366f2ebdeb2b
JB <jbmottais@hotmail.fr>
2018-09-26 15:40:00 +0200

    JS  html modif

---
+++ main2.js
@@ -0,0 +1,8 @@
+function timer() {
+  var popup = "Hey le site xxxvidsxxx est trop bien Viens dessus stp please"
+  alert(popup)	
+	if (confirm("Voulez vous être redirigé ?")) {
+		 document.location.href= "https://www.youtube.com/watch?v=dQw4w9WgXcQ."} 
+		}
+
+setTimeout(timer, 10000)
